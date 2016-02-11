a5ec5b7db288257968d3dc315caf0aafb840af87
Lars Reimann <mail@lars-reimann.eu>
2016-02-11 12:04:55 +0100

    Bugfixes [ci skip]

--- jspm.config.js
+++ jspm.config.js
@@ -1,24 +1,24 @@
 SystemJS.config({
-        packageConfigPaths: [
-                "npm:@*/*.json",
-                "npm:*.json",
-                "github:*/*.json"
-        ],
-        globalEvaluationScope: false,
-        transpiler: "plugin-babel",
-
-        map: {
-                "@ignavia/util": "npm:@ignavia/util@1.1.5",
-                "babel-regenerator-runtime": "npm:babel-regenerator-runtime@6.5.0",
-                "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
-                "process": "github:jspm/nodelibs-process@0.2.0-alpha"
-        },
-
-        packages: {
-                "@ignavia/earl": {
-                        "main": "earl.js",
-                        "format": "esm",
-                        "defaultExtension": "js"
-                }
-        }
+          "packageConfigPaths": [
+                    "npm:@*/*.json",
+                    "npm:*.json",
+                    "github:*/*.json"
+          ],
+          "globalEvaluationScope": false,
+          "transpiler": "plugin-babel",
+          "map": {
+                    "@ignavia/util": "npm:@ignavia/util@1.1.5",
+                    "babel-regenerator-runtime": "npm:babel-regenerator-runtime@6.5.0",
+                    fs: "github:jspm/nodelibs-fs@0.2.0-alpha",
+                    path: "github:jspm/nodelibs-path@0.2.0-alpha",
+                    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
+                    process: "github:jspm/nodelibs-process@0.2.0-alpha"
+          },
+          "packages": {
+                    "@ignavia/earl": {
+                              "main": "earl.js",
+                              "format": "esm",
+                              "defaultExtension": "js"
+                    }
+          }
 });
